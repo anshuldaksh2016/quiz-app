d9751555ae8c4e92077577718bbaac023c57da06
anshuldaksh2016 <anshuldaksh2016@gmail.com>
2020-12-03 13:56:14 +0530

    completed quiz app

--- src/App.js
+++ src/App.js
@@ -4,17 +4,52 @@ import './App.css'
 export default function App() {
   {/* create state variable for questions tracking  */ }
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [quizFinished, setQuizFinished] = useState(false);
+
+  function clickHandler() {
+
+    if (currentIndex === Question.length - 1) {
+      setQuizFinished(true);
+    }
+    setCurrentIndex(value => value + 1);
+  }
 
   return (
 
     <div className="App">
       <h1>Quiz App using React </h1>
-      <div className="questions">
-        {Question.map(ques => <h2>{ques.questionText}</h2>)}
-      </div>
+
+      {/* checking the quiz finished first */}
+
+      { quizFinished ? (<div className="score-section">You scored {currentIndex} out of {Question.length}</div>) :
+
+        (<>
+          <div className="questions-section">
+            <div className="question-count">
+              <span>Question {currentIndex}</span>
+            </div>
+            <div className="question-text">
+              {Question[currentIndex].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {Question[currentIndex].answerOptions.map(answer => {
+              return (
+                <button onClick={clickHandler} key={answer.answerText}>{answer.answerText}</button>
+              )
+            })}
+          </div>
 
 
-    </div>
+
+        </>
+        )
+      }
+
+
+
+
+    </div >
   );
 }
 
--- src/Data.js
+++ src/Data.js
@@ -38,3 +38,5 @@ const questions = [
 ]
 
 export default questions;
+
+
