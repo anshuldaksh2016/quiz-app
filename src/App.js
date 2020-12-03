c50e39a3843b3a4f79ffe4231af5063a8f223a64
anshuldaksh2016 <anshuldaksh2016@gmail.com>
2020-12-03 13:05:12 +0530

    questions rendered

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Question from './Data';
+import './App.css'
+export default function App() {
+  {/* create state variable for questions tracking  */ }
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-
-const App = () => {
   return (
+
     <div className="App">
+      <h1>Quiz App using React </h1>
+      <div className="questions">
+        {Question.map(ques => <h2>{ques.questionText}</h2>)}
+      </div>
+
 
     </div>
   );
 }
 
-export default App;
+
