45db12bee11f08c4297e4eee245f6cf5f0795c72
anshuldaksh2016 <anshuldaksh2016@gmail.com>
2020-12-03 12:53:07 +0530

    data added in json file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router , Route , Redirect , Switch } from 'react-router-dom'
-import NewPlace from './Places/pages/NewPlace';
-import Users from './user/pages/Users';
 
-const  App  = () => {
+
+const App = () => {
   return (
     <div className="App">
-      return <Router>
-      <Switch>
-        <Route path="/" exact>
-            <Users />
-        </Route>
-        <Route path="/places/new" exact>
-            <NewPlace />
-        </Route>
-        {/* redirect to / */}
-        <Redirect to="/" />
-        </Switch>
-      </Router>
+
     </div>
   );
 }
